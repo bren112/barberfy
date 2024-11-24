b6945457b4fcbfdb04ec83e0634d1b3c72e7608c
bren112 <brenomarinho1610@gmail.com>
2024-11-23 22:53:33 -0300

    Home / Login

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 import Home from "./pages/Home/Home";
-import Sobre from "./pages/Sobre/Sobre";
-import Atracoes from "./pages/Atracoes/Atracoes";
-import Pagamento from "./pages/Pagamento/Pagamento";
-import Teste from "./pages/Test/Test";
-import Avisos from "./pages/Avisos/Avisos";
 import Login from "./pages/Login/Index";
 
 function App() {
@@ -19,11 +14,6 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
-            <Route path="/sobre" element={<Sobre />} />
-            <Route path="/atracoes" element={<Atracoes />} />
-            <Route path="/pagamento" element={<Pagamento />} />
-            <Route path="/teste" element={<Teste />} />
-            <Route path="/avisos" element={<Avisos />} />
             <Route path="/login" element={<Login />} />
           </Routes>
         </div>
